76373b01cc54371220cb143821010347f526297c
tagiyevis <116768623+tagiyevis@users.noreply.github.com>
2023-08-29 07:49:14 +0400

    hover

--- src/Pages/Layiheler/Layiheler.jsx
+++ src/Pages/Layiheler/Layiheler.jsx
@@ -33,69 +33,69 @@ const item = {
 function Layiheler() {
     return (
         <>
-        <h1 className='alala' style={{textAlign:"center", fontWeight:"600"}}>Layihələr</h1>
-        <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            className='layihe'>
-                    
+            <h1 className='alala' style={{ textAlign: "center", fontWeight: "600" }}>Layihələr</h1>
             <motion.div
-                initial="hidden"
-                animate="visible"
-                variants={container}
-                className="layiheCenter">
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                className='layihe'>
+
                 <motion.div
-                    variants={item}
-                    className="layiheCart">
-                    <a target='_blank' href="https://salinaka.netlify.app/">
-                        <div className="layiheImg">
-                            <img src={Logo} />
-                        </div>
-                        <div className='layiheText'><p>Salinaka</p></div>
-                    </a>
-                </motion.div>
-                <motion.div
-                    variants={item}
-                    className="layiheCart">
-                    <a target='_blank' href="https://myshoppingisi.netlify.app">
-                        <div className=" layiheImg layiheImgmy">
-                            <img src={LogoShop} />
-                        </div>
-                        <div className='layiheText'><p>Myshopping</p></div>
-                    </a>
-                </motion.div>
-                <motion.div
-                    variants={item}
-                    className="layiheCart">
-                    <a target='_blank' href="https://todolistisi.netlify.app">
-                        <div className="layiheImg">
-                            <img src={Todolist} />
-                        </div>
-                        <div className='layiheText'><p>Todo list</p></div>
-                    </a>
-                </motion.div>
-                <motion.div
-                    variants={item}
-                    className="layiheCart">
-                    <a target='_blank' href="https://turboazisi.netlify.app">
-                        <div className="layiheImg turbo">
-                            <img src={Turbo} />
-                        </div>
-                        <div className='layiheText'><p>Turbo.az</p></div>
-                    </a>
-                </motion.div>
-                <motion.div
-                    variants={item}
-                    className="layiheCart">
-                    <a target='_blank' href="https://countryisi.netlify.app/">
-                        <div className="layiheImg turbo">
-                            <img src={country} />
-                        </div>
-                        <div className='layiheText'><p>Countries</p></div>
-                    </a>
+                    initial="hidden"
+                    animate="visible"
+                    variants={container}
+                    className="layiheCenter">
+                    <motion.div
+                        variants={item}
+                        className="layiheCart">
+                        <a target='_blank' href="https://salinaka.netlify.app/">
+                            <div className="layiheImg">
+                                <img src={Logo} />
+                            </div>
+                            <div className='layiheText'><p>Salinaka</p></div>
+                        </a>
+                    </motion.div>
+                    <motion.div
+                        variants={item}
+                        className="layiheCart">
+                        <a target='_blank' href="https://myshoppingisi.netlify.app">
+                            <div className=" layiheImg layiheImgmy">
+                                <img src={LogoShop} />
+                            </div>
+                            <div className='layiheText'><p>Myshopping</p></div>
+                        </a>
+                    </motion.div>
+                    <motion.div
+                        variants={item}
+                        className="layiheCart">
+                        <a target='_blank' href="https://todolistisi.netlify.app">
+                            <div className="layiheImg">
+                                <img src={Todolist} />
+                            </div>
+                            <div className='layiheText'><p>Todo list</p></div>
+                        </a>
+                    </motion.div>
+                    <motion.div
+                        variants={item}
+                        className="layiheCart">
+                        <a target='_blank' href="https://turboazisi.netlify.app">
+                            <div className="layiheImg turbo">
+                                <img src={Turbo} />
+                            </div>
+                            <div className='layiheText'><p>Turbo.az</p></div>
+                        </a>
+                    </motion.div>
+                    <motion.div
+                        variants={item}
+                        className="layiheCart">
+                        <a target='_blank' href="https://countryisi.netlify.app/">
+                            <div className="layiheImg turbo">
+                                <img src={country} />
+                            </div>
+                            <div className='layiheText'><p>Countries</p></div>
+                        </a>
+                    </motion.div>
                 </motion.div>
             </motion.div>
-        </motion.div>
         </>
     )
 }
